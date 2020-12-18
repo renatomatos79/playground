9246479e1e9be4a89ca25e5cec0101be911c0475
renato matos <renato.matos79@gmail.com>
2020-12-18 18:11:07 +0000

    step 0

---
+++ playground/MFC/MFC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = Environment.GetEnvironmentVariable("USER_NAME");
+            Console.WriteLine($"Hello {userName}!");
+            Console.WriteLine("Press ENTER to finish");
+            Console.ReadLine();
+        }
+    }
+}
