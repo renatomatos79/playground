befccae43ebed62d6e78c35b68842482c3b04861
renato matos <renato.matos79@gmail.com>
2020-12-18 22:38:08 +0000

    vs project

---
+++ vs/CoreDockerApi/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDockerApi
+{
+    public class ProductController : Controller
+    {
+        [HttpGet("/products")]
+        public async Task<IEnumerable<ProductModel>> GetProductsAsync()
+        {
+            return await Task.FromResult(new List<ProductModel> 
+            {
+                new ProductModel
+                {
+                    Id = 1,
+                    Name = "Eggs (box with 12 units)",
+                    Price = (decimal)1.50
+                },
+                new ProductModel
+                {
+                    Id = 2,
+                    Name = "Chocolate",
+                    Price = (decimal)1.99
+                }
+            });
+        }
+    }
+}
